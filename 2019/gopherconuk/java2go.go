8ddbaf694eaf36c289ac744509b29c5101f1b0a3
Horacio López <hdlopez@gmail.com>
2019-12-03 00:18:59 -0300

    Added new slides of golang best practices

---
+++ 2019/gopherconuk/java2go.go
@@ -0,0 +1,87 @@
+// START 1 OMIT
+package gopherconuk
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"sync"
+)
+
+// Private, available only from within the gopherconuk package
+type header struct {
+}
+
+// Public, available from other packages
+func Export() {
+}
+
+// Private, available only from within the gopherconuk package
+func doExport(h header) {
+}
+
+// END 1 OMIT
+
+// START 2 OMIT
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(p []byte) (n int, err error)
+}
+
+// ReadWriter is the interface that combines the Reader and Writer interfaces.
+type ReadWriter interface {
+	Reader
+	Writer
+}
+
+// END 2 OMIT
+
+// START 2.1 OMIT
+
+type Animal struct{}
+
+type Dog struct {
+	Animal
+}
+
+func main() {
+	var a Animal
+	a = Dog{}
+}
+
+// END 2.1 OMIT
+
+// START 3.1 OMIT
+
+type File struct {
+	sync.Mutex
+	rw io.ReadWriter
+}
+
+// END 3.1 OMIT
+
+func file() {
+	// START 3.2 OMIT
+
+	f := File{}
+	f.Lock()
+
+	// END 3.2 OMIT
+
+	// START 4 OMIT
+
+	var r io.Reader
+
+	r = bytes.NewBufferString("hello")
+
+	buf := make([]byte, 2048)
+	if _, err := r.Read(buf); err != nil {
+		log.Fatal(err)
+	}
+
+	// END 4 OMIT
+}
